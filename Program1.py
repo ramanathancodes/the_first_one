01439cb63bf13941aded9de0149ce9409ced57c7
ramanathancodes <summerguys.ram@gmail.com>
2024-01-11 14:58:22 +0530

    Initial

---
+++ Program1.py
@@ -0,0 +1,10 @@
+1. write a program to enter two integers and then perform all arithmetic operations on then
+
+'''
+get two integers 
+check they are integers 
+do all arithmetic and return outputs
+'''
+
+def two_int_math(num1,num2):
+
